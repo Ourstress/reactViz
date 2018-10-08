cfce9f472dc86f0b16ff13685c92b47b171f96ba
SG <sgng1@e.ntu.edu.sg>
2018-10-08 16:30:48 +0100

    first visualisation!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/react-vis/dist/style.css';
+import {XYPlot, MarkSeries, XAxis, YAxis, Hint} from 'react-vis';
+import ResalePriceIndexHDB from './DataApi/ResalePriceIndexHDB.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ResalePriceIndexHDB: [],
+      value: null
+  }
+  this._rememberValue = this._rememberValue.bind(this)
+  this._forgetValue = this._forgetValue.bind(this)
+}
+
+  async componentDidMount(){
+    try{
+      const HDBrpiItems = await ResalePriceIndexHDB()
+      this.setState({ResalePriceIndexHDB:HDBrpiItems})
+    } catch (error) {throw error}
+  }
+
+  _forgetValue() {
+    this.setState({
+      value: null
+    });
+  }
+
+  _rememberValue(value) {
+    this.setState({value});
+  }
+
   render() {
+    const {value} = this.state
+    const HDBrpi = this.state.ResalePriceIndexHDB.map((item)=> {
+      return {x: item.quarter, y: parseFloat(item.index)}
+    });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <XYPlot height={500} width={500} xType="ordinal">
+          <MarkSeries data={HDBrpi} onValueMouseOver={this._rememberValue} onValueMouseOut={this._forgetValue}/>
+          <XAxis tickValues={(HDBrpi.length > 15) ? HDBrpi.filter((item, idx) => {
+                  if ((idx % Math.floor(HDBrpi.length / 7)) === 0) {
+                    return item.x}
+                }).map(item => (item.x))
+              : HDBrpi.map(item => (item.x))}/>
+          <YAxis />
+          {value ? <Hint value={value} /> : null}
+        </XYPlot>
       </div>
     );
   }
---
+++ src/DataApi/ResalePriceIndexHDB.js
@@ -0,0 +1,7 @@
+export default async function ResalePriceIndexHDB() {  
+    const response = await fetch(
+      "https://data.gov.sg/api/action/datastore_search?limit=300&resource_id=52e93430-01b7-4de0-80df-bc83d0afed40"
+    );
+    const data = await response.json();
+    return data.result.records;
+  }
