fad5c786f26167387b0a656881361c4e439e903b
SG <sgng1@e.ntu.edu.sg>
2018-10-11 10:23:50 +0100

    trial look and feel of material ui menu and formgroup

---
+++ src/display/menu.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import CheckboxesGroup from './formGroup'
+
+class SimpleMenu extends React.Component {
+  state = {
+    anchorEl: null,
+  };
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { anchorEl } = this.state;
+
+    return (
+      <div>
+        <Button
+          aria-owns={anchorEl ? 'simple-menu' : null}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+        >
+          Open Menu
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose}
+        >
+          <CheckboxesGroup />
+        </Menu>
+      </div>
+    );
+  }
+}
+
+export default SimpleMenu;
